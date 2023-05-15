31685b6063484b3c6a133776ffaf2a3b8cf10575
Chris <rodcee13@gmail.com>
2023-05-15 09:46:17 -0400

    Step 3 Collecting the bet

--- project.js
+++ project.js
@@ -24,24 +24,39 @@ const deposit = () => {
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
       console.log("Invalid deposit amount, try again.");
     } else {
-        return numberDepositAmount;
+      return numberDepositAmount;
     }
   }
 };
 
-//Start of Step 2
+// Start of Step 2 Get number of bet lines
 const getNumberOfLines = () => {
-    while (true) {
-        const lines = prompt("Enter the number of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
-    
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-          console.log("Invalid number of lines, try again");
-        } else {
-            return numberOfLines;
-        }
-      }
-}
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
 
-const depositAmount = deposit();
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+// Collect a bet Step 3
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again");
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
